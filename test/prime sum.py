d397c0aed451eee7c50b164e3f6a84af7c7d201b
Administrator <Administrator>
2017-07-27 12:35:14 +0530

    the final code for prime consecutive

--- test/prime sum.py
+++ test/prime sum.py
@@ -1,11 +1,18 @@
 def prime(num):
-    primeflag=True
-    for i in range(2,num):
-        if num%i==0:
-            primeflag=False
+    primeflag = True
+    for i in range(2, num):
+        if num % i == 0:
+            primeflag = False
     if primeflag == True:
         return True
     else:
         return False
-for index in range(2, int(input())):
-    print(index," = ",prime(index))
+
+
+ans = 2
+no_of_iterations = int(input())
+for index in range(3, no_of_iterations):
+    if prime(index):
+        ans = ans + index
+        if ans <= no_of_iterations and prime(ans):
+         print(ans)
