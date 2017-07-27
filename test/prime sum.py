688f058707f50adc5c47e7fd0996f625140bdc27
Administrator <Administrator>
2017-07-27 11:51:38 +0530

    code fro first iteration

--- test/prime sum.py
+++ test/prime sum.py
@@ -1,6 +1,11 @@
 def prime(num):
-    primeflag=False
+    primeflag=True
+    for i in range(2,num):
+        if num%i==0:
+            primeflag=False
+            print(i)
     if primeflag== True:
         return True
-    else
-    return True
+    else:
+        return True
+prime(20)
